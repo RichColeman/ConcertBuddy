c33159445406cc7ee61fcf37d3654747670987ee
“IBayon“ <IBayon@Enova.com>
2018-12-30 14:35:46 -0600

    changes to profile page for testing user login

--- routes/PassportAPIroutes.js
+++ routes/PassportAPIroutes.js
@@ -6,18 +6,13 @@ var passport = require("../config/passport");
 
 //IB models and passport for authentication
 module.exports = function(app) {
-  // app.post('/api/login',
-  // passport.authenticate('local', { successRedirect: '/api/profile',
-  //                                  failureRedirect: '/api/login',
-  //                                  failureFlash: true }));
-
+// Using the passport.authenticate middleware with our local strategy.
+// If the user has valid login credentials, send them to the index page.
   app.post("/api/login", passport.authenticate("local"), function(req, res) {
-    // Since we're doing a POST with javascript, we can't actually redirect that post into a GET request
-    // So we're sending the user back the route to the members page because the redirect will happen on the front end
-    // They won't get this or even be able to access this page if they aren't authed
-    res.json("/profile");
+    
+    res.json("/");
   });
-  
+
   app.post("/api/signup", function(req, res) {
     console.log(req.body);
     db.User.create({
@@ -52,7 +47,10 @@ module.exports = function(app) {
     else {
       res.json({
         email: req.user.email,
-        id: req.user.id
+        id: req.user.id,
+        firstName: req.user.firstName,
+        lastName: req.user.lastName,
+        zipcode: req.user.zipcode
       });
     }
   });
