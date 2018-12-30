41f893d54038e6c7a354b10e04223b5468b26986
“IBayon“ <IBayon@Enova.com>
2018-12-30 13:30:38 -0600

    fixed login

--- routes/PassportAPIroutes.js
+++ routes/PassportAPIroutes.js
@@ -6,11 +6,18 @@ var passport = require("../config/passport");
 
 //IB models and passport for authentication
 module.exports = function(app) {
-  app.post('/api/login',
-  passport.authenticate('local', { successRedirect: '/api/profile.html',
-                                   failureRedirect: '/api/login',
-                                   failureFlash: true }));
+  // app.post('/api/login',
+  // passport.authenticate('local', { successRedirect: '/api/profile',
+  //                                  failureRedirect: '/api/login',
+  //                                  failureFlash: true }));
 
+  app.post("/api/login", passport.authenticate("local"), function(req, res) {
+    // Since we're doing a POST with javascript, we can't actually redirect that post into a GET request
+    // So we're sending the user back the route to the members page because the redirect will happen on the front end
+    // They won't get this or even be able to access this page if they aren't authed
+    res.json("/profile");
+  });
+  
   app.post("/api/signup", function(req, res) {
     console.log(req.body);
     db.User.create({
