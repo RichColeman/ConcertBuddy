35f8df145344508fac5f436ac3a39868b5357a8a
richcoleman <coleman.richardm@gmail.com>
2019-01-09 01:13:20 -0600

    got chat to function, styled event page and edited events.js

--- public/js/events.js
+++ public/js/events.js
@@ -1,7 +1,7 @@
-$(document).ready(function() {
+$(document).ready(function () {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
-  $.get("/api/user_data").then(function(data) {
+  $.get("/api/user_data").then(function (data) {
     $(".member-name").text(data.email);
     $(".member-fname").text(data.firstName);
     //in case we want to show these in the future
@@ -13,39 +13,61 @@ $(document).ready(function() {
   function eventAttendees() {
     let url = window.location.href;
     let eventId = url.split("events/")[1];
-    $.get(`/api/attendees/${eventId}`, function(data) {
-      console.log(data);
+    let usersGoing = [];
+    $.get(`/api/attendees/${eventId}`, function (users) {
+      users.forEach(user => {
+        usersGoing.push(user.firstName);
+      });
+      usersGoing.forEach(user => {
+        let userDiv = $("<div>");
+        userDiv.html(`<h3> ${user} is going.</h3>`)
+        $(".users").append(userDiv);
+      });
     })
-  }
-  
+  };
+
   function eventInfo() {
     let url = window.location.href;
     let eventId = url.split("events/")[1];
-    $.get(`/api/events/${eventId}`, function(data) {
-      console.log(data);
+    $.get(`/api/events/${eventId}`, function (concert) {
+      console.log(concert);
+      let concertDiv = $("<div>");
+      let headlineDiv = $("<div>");
+      let modalDiv = $("<h5>");
+      modalDiv.addClass("modal-title");
+      concertDiv.html(`<h3> ${concert.artist} is playing at the ${concert.venue} in ${concert.city}. Check out who's going then head to the chat. </h3>`)
+      $(".concert-data").append(concertDiv);
+      headlineDiv.html(`<h2>Congrats, <span class="member-fname"></span>. <br> You're one step closer to finding your
+      ConcertBuddy for the ${concert.artist} show.</h2><br><br>`);
+      $(".headline").append(headlineDiv);
+      modalDiv.html(`Chat for ${concert.artist} at ${concert.venue}</h5>`);
+      $(".modal-header").prepend(modalDiv);
       initMap(data.Latitude, data.Longitude)
-  })
+    })
 
-  function initMap(latitude, longitude) {
-    var myLatLng = { lat: latitude, lng: longitude };
-  
-    var map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      center: myLatLng
-    });
-  
-    var marker = new google.maps.Marker({
-      position: myLatLng,
-      map: map,
-      title: "Hello World!"
-    });
+    function initMap(latitude, longitude) {
+      var myLatLng = {
+        lat: latitude,
+        lng: longitude
+      };
+
+      var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 4,
+        center: myLatLng
+      });
+
+      var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: "Hello World!"
+      });
+    }
+    initMap();
+
   }
-  initMap();
 
-}
-
-eventAttendees();
-eventInfo();
+  eventAttendees();
+  eventInfo();
 });
 // $(document).ready(function() {
 //   // This file just does a GET request to figure out which user is logged in
