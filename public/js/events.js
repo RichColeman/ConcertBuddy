31d297eeaeb215d7c3acec049d28cac7c02adf11
Andrew Andersen <andersac88@gmail.com>
2019-01-09 02:23:01 -0600

    map

--- public/js/events.js
+++ public/js/events.js
@@ -22,7 +22,10 @@ $(document).ready(function () {
         let userDiv = $("<div>");
         userDiv.html(`<h3> ${user} is going.</h3>`)
         $(".users").append(userDiv);
+     
       });
+      let elem = document.getElementsByClassName("users");
+      elem.scrollIntoView(false)
     })
   };
 
@@ -42,7 +45,7 @@ $(document).ready(function () {
       $(".headline").append(headlineDiv);
       modalDiv.html(`Chat for ${concert.artist} at ${concert.venue}</h5>`);
       $(".modal-header").prepend(modalDiv);
-      initMap(data.Latitude, data.Longitude)
+      initMap(+concert.Latitude, +concert.Longitude)
     })
 
     function initMap(latitude, longitude) {
@@ -52,8 +55,169 @@ $(document).ready(function () {
       };
 
       var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 4,
-        center: myLatLng
+        zoom: 10,
+        styles: [
+          {
+            "elementType": "geometry",
+            "stylers": [
+              {
+                "color": "#f5f5f5"
+              }
+            ]
+          },
+          {
+            "elementType": "labels.icon",
+            "stylers": [
+              {
+                "visibility": "off"
+              }
+            ]
+          },
+          {
+            "elementType": "labels.text.fill",
+            "stylers": [
+              {
+                "color": "#616161"
+              }
+            ]
+          },
+          {
+            "elementType": "labels.text.stroke",
+            "stylers": [
+              {
+                "color": "#f5f5f5"
+              }
+            ]
+          },
+          {
+            "featureType": "administrative.land_parcel",
+            "elementType": "labels.text.fill",
+            "stylers": [
+              {
+                "color": "#bdbdbd"
+              }
+            ]
+          },
+          {
+            "featureType": "poi",
+            "elementType": "geometry",
+            "stylers": [
+              {
+                "color": "#eeeeee"
+              }
+            ]
+          },
+          {
+            "featureType": "poi",
+            "elementType": "labels.text.fill",
+            "stylers": [
+              {
+                "color": "#757575"
+              }
+            ]
+          },
+          {
+            "featureType": "poi.park",
+            "elementType": "geometry",
+            "stylers": [
+              {
+                "color": "#e5e5e5"
+              }
+            ]
+          },
+          {
+            "featureType": "poi.park",
+            "elementType": "labels.text.fill",
+            "stylers": [
+              {
+                "color": "#9e9e9e"
+              }
+            ]
+          },
+          {
+            "featureType": "road",
+            "elementType": "geometry",
+            "stylers": [
+              {
+                "color": "#ffffff"
+              }
+            ]
+          },
+          {
+            "featureType": "road.arterial",
+            "elementType": "labels.text.fill",
+            "stylers": [
+              {
+                "color": "#757575"
+              }
+            ]
+          },
+          {
+            "featureType": "road.highway",
+            "elementType": "geometry",
+            "stylers": [
+              {
+                "color": "#dadada"
+              }
+            ]
+          },
+          {
+            "featureType": "road.highway",
+            "elementType": "labels.text.fill",
+            "stylers": [
+              {
+                "color": "#616161"
+              }
+            ]
+          },
+          {
+            "featureType": "road.local",
+            "elementType": "labels.text.fill",
+            "stylers": [
+              {
+                "color": "#9e9e9e"
+              }
+            ]
+          },
+          {
+            "featureType": "transit.line",
+            "elementType": "geometry",
+            "stylers": [
+              {
+                "color": "#e5e5e5"
+              }
+            ]
+          },
+          {
+            "featureType": "transit.station",
+            "elementType": "geometry",
+            "stylers": [
+              {
+                "color": "#eeeeee"
+              }
+            ]
+          },
+          {
+            "featureType": "water",
+            "elementType": "geometry",
+            "stylers": [
+              {
+                "color": "#c9c9c9"
+              }
+            ]
+          },
+          {
+            "featureType": "water",
+            "elementType": "labels.text.fill",
+            "stylers": [
+              {
+                "color": "#9e9e9e"
+              }
+            ]
+          }
+        ],
+        center: myLatLng,
+        disableDefaultUI: true
       });
 
       var marker = new google.maps.Marker({
@@ -62,7 +226,6 @@ $(document).ready(function () {
         title: "Hello World!"
       });
     }
-    initMap();
 
   }
 
